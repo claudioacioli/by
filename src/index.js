69c704622f670815a703580d14c2f1a2046330db
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-17 22:41:48 -0300

    feat: Create core code

---
+++ src/index.js
@@ -0,0 +1,25 @@
+export const
+  
+  byId = (id, parent=document) => 
+    parent.getElementById(id)
+  ,
+
+  byName = (name, parent=document) => 
+    parent.getElementsByName(name)
+  ,
+
+  byClass = (className, parent=document) => 
+    parent.getElementsByClassName(className)
+  ,
+
+  byTag = (tag, parent=document) => 
+    parent.getElementsByTagName(tag)
+  .
+
+  bySelector = (selector, parent=document) =>
+    parent.querySelector(selector)
+  ,
+
+  byAll = (selector, parent=document) =>
+    parent.querySelectorAll(selector)
+;
